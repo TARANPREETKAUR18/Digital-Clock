0a5badf553db912f4e17f3926ecfde7bdaf4842d
taranpreetkaur18 <taranpreet145btcsai21@igdtuw.ac.in#>
2024-04-14 00:35:56 +0530

    Added Date and Day

--- script.js
+++ script.js
@@ -1,3 +1,63 @@
+const clock = () => {
+  let date = new Date()
+}
+
+/*=============== TIME AND DATE TEXT ===============*/
+const dateDayWeek = document.getElementById('date-day-week'),
+      dateMonth = document.getElementById('date-month'),
+      dateDay = document.getElementById('date-day'),
+      dateYear = document.getElementById('date-year'),
+      textHour = document.getElementById('text-hour'),
+      textMinutes = document.getElementById('text-minutes'),
+      textAmPm = document.getElementById('text-ampm')
+
+const clockText = () =>{
+   // We get the Date object
+   let date = new Date()
+
+   // We get the time and date
+   let dayWeek = date.getDay(),
+       month = date.getMonth(),
+       day = date.getDate(),
+       year = date.getFullYear(),
+       hh = date.getHours(),
+       mm = date.getMinutes(),
+       ampm
+
+   // We get the days of the week and the months. (First day of the week Sunday)
+   let daysWeek = ['Sunday', 'Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday']
+   let months = ['Jan', 'Feb', 'Mar', 'Apr', 'May', 'Jun', 'Jul', 'Aug', 'Sep', 'Oct', 'Nov', 'Dec']
+
+   // We add the corresponding dates
+   dateDayWeek.innerHTML = `${daysWeek[dayWeek]}`
+   dateMonth.innerHTML = `${months[month]}`
+   dateDay.innerHTML = `${day}, `
+   dateYear.innerHTML = year
+
+   if(hh >= 12){
+    hh = hh - 12
+    ampm = 'PM'
+ } else{
+    ampm = 'AM'
+ }
+
+ textAmPm.innerHTML = ampm
+
+ // When it is 0 hours (early morning), we tell it to change to 12 hours
+ if(hh == 0){hh = 12}
+
+ // If hours is less than 10, add a 0 (01,02,03...09)
+ if(hh < 10){hh = `0${hh}`}
+
+ textHour.innerHTML = `${hh}:`
+
+ // If minutes is less than 10, add a 0 (01,02,03...09)
+ if(mm < 10){mm = `0${mm}`}
+
+ textMinutes.innerHTML = mm
+}
+setInterval(clockText, 1000) // (Updates every 1s) 1000 = 1s
+
 // Clock
 
 const updateTime = () => {
